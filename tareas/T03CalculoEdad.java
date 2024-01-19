83ef109821e4bc709f85d744f007d37aa20587bb
benja-pizarro <eduardo.latorrevandeperre@wbdcontractor.com>
2024-01-19 11:09:03 -0300

    Revisión de tareas

--- tareas/T03CalculoEdad.java
+++ tareas/T03CalculoEdad.java
@@ -1,5 +1,7 @@
 package tareas;
 
+import java.util.Scanner;
+
 public class T03CalculoEdad {
     /*
     Pedir al usuario el año en que nació y el año actual, 
@@ -14,9 +16,32 @@ public class T03CalculoEdad {
     debe mostrar un mensaje.
 
     Imprimir la edad del usuario y si es mayor de edad
+
+    Observaciones de revisión:
+    - Siempre utiliza minúculas como letra inicial de una variable
+        ej: annoNacimiento
+    - Por un tema de formato, utiliza la función print en vez de println 
     */
 
     public static void main(String[] args) {
-        
+        int AmoNacimiento;
+        int AmoActual;
+        int edad;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el año de su nacimiento: ");
+        AmoNacimiento = scanner.nextInt();
+        System.out.print("Ingrese el año actual: ");
+        AmoActual = scanner.nextInt();
+        edad = AmoActual - AmoNacimiento;
+        if(edad < 18 && edad > 0){
+            System.out.println("Usted tiene " + edad + " y es menor de edad");
+        }else if(edad >= 18 && edad < 100){
+            System.out.println("Usted tiene " + edad + " y es mayor de edad");
+        }else if(edad > 100 || edad < 0){
+            System.out.println("Los valores ingresados son invalidos");
+        }
+
+        scanner.close();
     }
 }
--- tareas/T04NumerosPares.java
+++ tareas/T04NumerosPares.java
@@ -1,5 +1,7 @@
 package tareas;
 
+import java.util.Scanner;
+
 public class T04NumerosPares {
     /* 
         Determinar si un número es par o impar
@@ -15,6 +17,23 @@ public class T04NumerosPares {
     */
 
     public static void main(String[] args) {
-        
+        double num1;
+        double resultadoDouble;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero para saber si es par o impar: ");
+        num1 = scanner.nextDouble();
+        if(num1 < 0){
+                System.out.println("El numero ingresado es invalido");
+            }
+
+        resultadoDouble = num1%2;
+        if(resultadoDouble == 0){
+            System.out.println("Su numero es par");
+        }else if(resultadoDouble > 0){
+            System.out.println("Su numero es impar");
+        }
+
+        scanner.close();
     }
 }
