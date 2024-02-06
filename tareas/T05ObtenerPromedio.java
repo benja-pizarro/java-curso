30910f053fb9c2f16257b369c3a955d8590ef2ce
benja-pizarro <benjaminpizarrogalvez80@gmail.com>
2024-02-06 17:22:43 -0300

    Promedio tarea completa

--- tareas/T05ObtenerPromedio.java
+++ tareas/T05ObtenerPromedio.java
@@ -1,5 +1,7 @@
 package tareas;
 
+import java.util.Scanner;
+
 /* 
     Realizar un programa que permita al usuario ingresar 7 notas
     y una vez ingresadas, calcular el promedio de estas
@@ -11,6 +13,21 @@ package tareas;
 */
 public class T05ObtenerPromedio {
     public static void main(String[] args) {
-        //TODO
+        
+        double[] nota = new double[7];
+        int indice = 0;
+        double resultadoSuma;
+        double promedio;
+        Scanner Scanner = new Scanner(System.in);
+
+        while(indice < nota.length){
+            System.out.print("Ingrese una nota: ");
+            nota[indice] = Scanner.nextDouble();
+            indice++;
+        }
+        resultadoSuma = nota[0] + nota[1] + nota[2] + nota[3] + nota[4] + nota[5] + nota[6];
+        promedio = resultadoSuma/nota.length;
+        System.out.print("resultado del promedio:" + promedio);
+
+        }
     }
-}
